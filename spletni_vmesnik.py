46591e610b64feea19544cbb644d63e399c877a8
Matej Šafarič <matej.safaric@gmail.com>
2022-08-29 16:06:32 +0200

    Manjsi random popravki ki si ne zasluzijo locenih commitov

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,6 +1,6 @@
 import bottle
 import model
-from datetime import date
+from datetime import date, timedelta, datetime
 
 root = model.Root([], [], [], False, None)
 print('Nalagam datoteke...')
@@ -9,30 +9,10 @@ print('Ustvarjam ure...')
 root.pripravi_ure()
 print('Pripravljeno!')
 
-@bottle.get("/")
-def zacetna_stran(teden: int=date.today().isocalendar()[1]):
-    try:
-        pristevek_tednu = bottle.request.forms['teden']
-    except:
-        naslednji_teden = 0
-        prejsnji_teden = 0
-    instruktor_bool = bottle.request.get_cookie('instruktor_bool')
-    if instruktor_bool == None:
-        return bottle.template(
-            'home.html',
-            uporabniki=root.uporabniki,
-            vrstice = root.pripravi_urnik_html_tabela(teden, root.seznam_instruktorjev()[0])
-            )
-    elif int(instruktor_bool):
-        return bottle.template('home_instruktor.html')
-    else:
-        return bottle.template('home_ucenec.html')
-    
-
-@bottle.post('/prijava')
+@bottle.post('/prijava/')
 def prijava():
-    username = bottle.request.forms['username']
-    password = bottle.request.forms['password']
+    username = bottle.request.forms.getunicode('username')
+    password = bottle.request.forms.getunicode('password')
     (uporabnik, uspesnost_prijave) = root.preveri_prijavo(username, password)
     if uporabnik == None:
         return 'Ta uporabnik ne obstaja'
@@ -42,24 +22,9 @@ def prijava():
         instruktor_bool = '1' if uporabnik.instruktor else '0'
         bottle.response.set_cookie('username', username, path='/')
         bottle.response.set_cookie('instruktor_bool', instruktor_bool, path='/')
-        bottle.redirect('/')
+        bottle.redirect('/urnik/')
 
-@bottle.get("/urnik")
-def urnik():
-    return bottle.template(
-        'urnik.html',
-        vrstice = root.pripravi_urnik_html_tabela(model.date.today().isocalendar()[1], root.seznam_instruktorjev()[0])
-        )
-
-@bottle.get("/urnik/<id_instruktorja: int>")
-def urnik_instruktor(id_instruktorja):
-    return bottle.template(
-        'urnik.html',
-        seznam_instruktorjev = root.seznam_instruktorjev(),
-        vrstice = root.pripravi_urnik_html_tabela(model.date.today().isocalendar()[1], root.najdi_uporabnika_id(id_instruktorja))
-    )
-
-@bottle.get("/log-in")
+@bottle.get("/log-in/")
 def log_in():
     return bottle.template(
         'log-in.html'
@@ -79,18 +44,13 @@ def zacetna_stran():
         seznam_instruktorjev = root.seznam_instruktorjev()
         )
 
-@bottle.get("/urnik")
+@bottle.get("/urnik/")
 def urnik():
     try:
         id_instruktorja = bottle.request.query['instruktorji']
     except KeyError:
         id_instruktorja = 1
     bottle.redirect(f"/urnik/{id_instruktorja}/{model.date.today().isocalendar()[0]}/{model.date.today().isocalendar()[1]}/")
-    # return bottle.template(
-    #     "urnik.html",
-    #     vrstice = root.pripravi_urnik_html_tabela(model.date.today().isocalendar()[1], root.najdi_uporabnika_id(id_instruktorja)),
-    #     seznam_instruktorjev = root.seznam_instruktorjev()
-    # )
 
 @bottle.get("/urnik/<id_instruktorja:int>/<leto:int>/<teden:int>/")
 def urnik2(id_instruktorja, leto=model.date.today().isocalendar()[0], teden=model.date.today().isocalendar()[1]):
@@ -169,7 +129,8 @@ def razpolozi():
     uporabnik = root.najdi_uporabnika_username(uporabnik_username)
     for ura in ure:
         root.najdi_uro(ura).razpolozi(uporabnik)
-    return bottle.redirect("/urnik/")
+    root.shrani_ure('ure.json')
+    bottle.redirect("/urnik/")
 
 @bottle.get("/cancel/")
 def cancel():
@@ -207,6 +168,7 @@ def odpovej():
 
 @bottle.get("/osebne_ure/")
 def osebne_ure():
+    if isinstance(bottle.request.get_cookie('username'), str):
         uporabnik_username = bottle.request.get_cookie('username')
         uporabnik = root.najdi_uporabnika_username(uporabnik_username)
         trenuten_cas = datetime.today()
