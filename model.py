19423ab450e052a15f2f1ce8532268108f9c04a7
Matej Šafarič <matej.safaric@gmail.com>
2022-08-31 22:46:34 +0200

    Zbrisal komentarje in funkcije v modelu ki jih spletni vmesnik ne uporablja

--- model.py
+++ model.py
@@ -1,13 +1,7 @@
-#Root class (INS) bo class ki vsebuje vse podatke, ki so na voljo:
-#   ure, uporabnike (ucence in ucitelje), predmete, ?dneve?, 
-#
-
 from dataclasses import dataclass
 from datetime import date, datetime, timedelta
 import json
 from typing import List
-
-
 
 
 
@@ -49,11 +43,8 @@ class Uporabnik:
 @dataclass
 class Predmet:
     ime: str
-    stopnja: int
+    stopnja: int    #0 - OŠ, 1 - SŠ, 2 - FK
     id: int
-    #0 - OŠ
-    #1 - SŠ
-    #2 - FK
     
     def __str__(self):
         moznosti = [(0, 'OŠ'),(1, 'SŠ'),(2, 'FK')]
@@ -88,15 +79,14 @@ class Ura:
     #   0 - obstoj ure;
     #   1 - ura je razpisana
     #   2 - ura je rezervirana
-    #   3 - ura je opravljena/izvedena ????
     predmet: Predmet
-    ucenec: Uporabnik #ID stevilka ucencu prirejenega racuna
+    ucenec: Uporabnik
     instruktor: Uporabnik
     id: int
 
     def __str__(self):
         if self.stopnja_zasedenosti == 0:
-            return '###'
+            return ''
         elif self.stopnja_zasedenosti == 1:
             return 'Prost termin'
         else:
@@ -149,17 +139,6 @@ class Root:
     ure: List[Ura]
     uporabniki: List[Uporabnik]
     predmeti: List[Predmet]
-    prijavljenost: bool
-    prijavljenec: Uporabnik
-
-    def v_slovar(self):
-        return {
-            'ure': [ura.v_slovar() for ura in self.ure],
-            'uporabniki': [uporabnik.v_slovar() for uporabnik in self.uporabniki],
-            'predmeti': [predmet.v_slovar() for predmet in self.predmeti],
-            'prijavljenost': self.prijavljenost,
-            'prijavljenec': self.prijavljenec.v_slovar() if self.prijavljenec != None else None
-        }
 
     def nalozi_datoteke(self, predmeti: str, uporabniki: str, ure: str):
         """Ta funkcija prenese podatke iz .json datotek v root"""
@@ -185,24 +164,6 @@ class Root:
                 zadnji_ponedeljek = datetime.fromisocalendar(zadnji_datum_v_sistemu.isocalendar()[0], zadnji_datum_v_sistemu.isocalendar()[1], 1)
                 self.ustvari_dan_praznih_ur(zadnji_ponedeljek + timedelta(days=i, weeks=1))
         self.shrani_ure('ure.json')
-
-    def pripravi_urnik_za_print(self, teden: int, instruktor: Uporabnik):
-        """Ta funkcija pripravi urnik enega tedna za enega instruktorja in ga returna kot seznam vrstic"""
-        seznam_instruktorjevih_ur = [ura for ura in self.ure if ura.instruktor == instruktor]
-        seznam_instruktorjevih_ur_v_tem_tednu = [ura for ura in seznam_instruktorjevih_ur if ura.cas.isocalendar()[1] == teden]
-        vrstice_urnika = []
-        for i in range(8, 20):
-            vrstica = f'| {i:>2}:00 |'
-            for ura in seznam_instruktorjevih_ur_v_tem_tednu:
-                if ura.cas.hour == i:
-                    if ura.stopnja_zasedenosti == 0:
-                        vrstica += '     ###     |'
-                    elif ura.stopnja_zasedenosti == 1:
-                        vrstica += ' prost termin |'
-                    else:
-                        vrstica += '   zasedeno   |'
-            vrstice_urnika.append(vrstica)
-        return vrstice_urnika
 
     def pripravi_urnik_html_tabela_instruktor(self, leto: int, teden: int, instruktor: Uporabnik):
         """Ta funkcija naredi seznam celic za html tabelo ki prikazuje urnik enega tedna za enega instruktorja"""
@@ -238,10 +199,6 @@ class Root:
     def seznam_instruktorjev(self):
         return [uporabnik for uporabnik in self.uporabniki if uporabnik.instruktor]
 
-    def v_datoteko(self, datoteka: str):
-        with open(datoteka, 'w', encoding='UTF-8') as dat:
-            json.dump(self.v_slovar, dat, indent=4)
-
     def shrani_ure(self, datoteka: str):
         with open(datoteka, 'w', encoding='UTF-8') as dat:
             json.dump([ura.v_slovar() for ura in self.ure], dat, indent=4)
@@ -253,11 +210,6 @@ class Root:
     def shrani_predmete(self, datoteka: str):
         with open(datoteka, 'w', encoding='UTF-8') as dat:
             json.dump([predmet.v_slovar() for predmet in self.predmeti], dat, indent=4)
-
-    def ustvari_prazno_uro(self, cas:datetime):
-        zadnji_id = self.ure[-1].id
-        self.ure.append(Ura(cas, 0, None, None, None, zadnji_id + 1))
-        self.shrani_ure('ure.json')
 
     def ustvari_dan_praznih_ur(self, datum:date):
         zadnji_id = self.ure[-1].id
@@ -327,72 +279,3 @@ class Root:
         for ura in self.ure:
             if ura.id == id:
                 return ura 
-    
-
-# Ustvari funkcijo ki naredi dovolj ur za en dan od 8ih do 20ih npr         X
-# Razmisli ali je bolje dodati se en class Dan za organizacijo ur   NE
-
-# Podatki naj se sproti shranjujejo na zunanji file ki se prebere ob zagonu programa
-# V funkciji za ustvarjanje novih ur dodaj shranjevanje ur v zunanji .txt file      X
-# Razmisli/naredi enako za uporabnike in predmete
-# Nadaljuj z implementacijo log-in in uporabe id-jev
-# Koncaj metodo find(id)        X
-
-# Podatki se morajo sproti shranjevati ker bo v nasprotnem primeru ob crashu vse novo izgubljeno!!!
-# Ko spremenis atribut objekta se mora spremeniti tudi zapis v txt file-u
-# PAZI NA ID stevilke ker se zacnejo z 1
-
-#Preden s podatkom kaj naredis, preveri ali sploh obstaja v txt.file-u / root-u
-
-# primer = Root(    
-#     [
-#         Ura(datetime(2022, 8, 9, 8), 2, Predmet('fizika', 2, 2), Uporabnik(('Alfi', 'Snific'), 'Ucko', '4321', 2, False), Uporabnik(('Safaric', 'Matej'), 'Prof', '1234', 1, True), 2),
-#         Ura(datetime(1, 1, 1, 10), 0, None, None, None, 3)
-#     ],
-#     [
-#         Uporabnik(('Safaric', 'Matej'), 'Prof', '1234', 1, True),
-#         Uporabnik(('Alfi', 'Snific'), 'Ucko', '4321', 2, False)
-#     ],
-#     [
-#         Predmet('matematika', 1, 1),
-#         Predmet('fizika', 2, 2)
-#     ],
-#     False,
-#     None
-# )
-
-# # primer = Root(
-# #     [
-# #         Ura(datetime(1, 1, 1, 9), 1, Predmet('matematika', 1, 1), Uporabnik(('Safaric', 'Matej'), 'Prof', '1234', 1, True), 1)
-# #     ],
-# #     [
-# #         Uporabnik(('Safaric', 'Matej'), 'Prof', '1234', 1, True)
-# #     ],
-# #     [
-# #         Predmet('matematika', 1, 1)
-# #     ]
-# #     )
-
-# for i in primer.ure:
-#     print(i)
-
-# print('\n')
-# primer.ustvari_dan_praznih_ur(date(2022,12,25))
-# print('\n')
-# #primer.najdi_uro(1).pocisti()
-# print('\n')
-
-
-
-# for i in primer.ure:
-#     print(i)
-
-# primer.shrani_predmete('predmeti.json')
-# primer.shrani_uporabnike('uporabniki.json')
-# primer.shrani_ure('ure.json')
-
-    
-
-
-
-
